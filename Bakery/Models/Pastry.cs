2a34fa6f7cdb7d27c54cbcac30c62c139fc5dad0
Tyler Adams <skipmarcel@gmail.com>
2023-09-09 20:35:03 -0700

    add PastryDeal class to Pastry.cs

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -2,6 +2,20 @@ namespace Bakery.Models
 {
  public class PastryDeal
  {
-  
+  private decimal _pricePerPastry;
+  private int _numPastries;
+
+  public PastryDeal(decimal pricePerPastry, int numPastries)
+  {
+   _pricePerPastry = pricePerPastry;
+   _numPastries = numPastries;
+  }
+
+  public decimal GetTotalPastryPrice()
+  {
+   int numPaidPastries = _numPastries / 4 * 3 + _numPastries % 4;
+   decimal totalPrice = numPaidPastries * _pricePerPastry;
+   return totalPrice;
+  }
  }
 }
