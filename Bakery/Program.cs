eaf66d278a0999fe6b54204f5371a9b7903c896c
Tyler Adams <skipmarcel@gmail.com>
2023-09-09 21:00:55 -0700

    add program file for console app

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,9 +9,34 @@ namespace Bakery
   {
     static void Main()
     {
+Console.WriteLine("                  ");
+Console.WriteLine("             ~*** WELCOME TO ***~");
+string welcomeBanner = WelcomeBanner.Welcome;
+Console.WriteLine(welcomeBanner);
 
+Console.WriteLine("                  ");
+Console.WriteLine("Our bread loaves are $5 - Buy 2 get 1 free!");
+Console.WriteLine("Our fruit pastries are $2 - Buy 3 get 1 free!");
 
-}
+Console.WriteLine("                  ");
+Console.WriteLine("How many loaves of bread  would you like?");
+
+string breadCount = Console.ReadLine();
+int totalLoaves = int.Parse(breadCount);
+BreadDeal deal = new BreadDeal(5.0m, totalLoaves);
+decimal totalPrice = deal.GetTotalPrice();
+Console.WriteLine($"Total for your bread: ${totalPrice:F2}");
+Console.WriteLine("                  ");
+Console.WriteLine("How many fruit pastries would you like?");
+
+string pastryCount = Console.ReadLine();
+int totalPastries = int.Parse(pastryCount);
+PastryDeal pDeal = new PastryDeal(2.0m, totalPastries);
+decimal totalPastryPrice = pDeal.GetTotalPastryPrice();
+Console.WriteLine($"Total for your pastries: ${totalPastryPrice:F2}");
+Console.WriteLine($"Your grand total is: ${totalPastryPrice + totalPrice:F2}");    
+Console.WriteLine("Thank you for coming! Please tell a friend");     
+    }
 
   }
 }
