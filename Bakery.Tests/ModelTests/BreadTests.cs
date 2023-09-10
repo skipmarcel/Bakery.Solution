96c5a0e81fa64e419f0aa2ae4ae7bbf5a66d1c09
Tyler Adams <skipmarcel@gmail.com>
2023-09-09 19:24:46 -0700

    add first failed test

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -1,2 +1,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
+
+namespace Bakery.Tests 
+{
+ [TestClass]
+ public class BreadTests
+ {
+
+  [TestMethod]
+  public void GetTotalPrice_ReturnsCorrectTotalPriceForOneLoafOfBread()
+  {
+   //Arrange 
+   decimal pricePerLoaf = 5.0m;
+   int numLoaves = 1;
+   BreadDeal breadDeal = new BreadDeal(pricePerLoaf, numLoaves);
+
+   //Act
+   decimal totalPrice = breadDeal.GetTotalPrice();
+
+   //Assert should be the same becaue it's only one loaf
+   Assert.AreEqual(pricePerLoaf, totalPrice);
+  }
+ }
+}
